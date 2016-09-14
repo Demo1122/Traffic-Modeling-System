af29c902d75309b46dbeef5cecfbe163b5f21f01
Demo1122 <h43730@mvrht.com>
2016-09-13 22:29:34 -0500

    Create new class Rules

---
+++ src/com/nwmissouri/vo/Rules.java
@@ -0,0 +1,29 @@
+package com.nwmissouri.vo;
+
+public class Rules {
+
+	private float speed;
+	private String diretion;
+	
+	public String getDiretion() {
+		return diretion;
+	}
+	public void setDiretion(String diretion) {
+		this.diretion = diretion;
+	}
+	public float getSpeed() {
+		return speed;
+	}
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	public String getRuleName() {
+		return ruleName;
+	}
+	public void setRuleName(String ruleName) {
+		this.ruleName = ruleName;
+	}
+	private String ruleName;
+	
+	
+}
